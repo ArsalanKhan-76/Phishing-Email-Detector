a74b45ef35f463ad68c1264553ef4730a727aec4
ArsalanKhan-76 <wwwak374@@gmail.com>
2026-04-03 11:57:56 +0530

    Clean up: remove unused files and IDE folder

--- tfidf_model.py
+++
@@ -1,8 +0,0 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-text = ["This is a sample email", "Another example for fraud detection"]
-
-vectorizer = TfidfVectorizer()
-X = vectorizer.fit_transform(text)
-
-print(X.toarray())
